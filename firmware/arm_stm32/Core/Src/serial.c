7ade39fcc7b9175ef8ccaa740e650b4b28a2a683
BennyG9 <bennygoldstein99@gmail.com>
2026-06-23 14:26:14 -0700

    added timout to firmware serial read functions

--- firmware/arm_stm32/Core/Src/serial.c
+++ firmware/arm_stm32/Core/Src/serial.c
@@ -19,14 +19,14 @@ void Serial_Init(UART_HandleTypeDef* huart){
 // returns HAL_OK if the byte was read
 HAL_StatusTypeDef Serial_ReadByte(uint8_t* byte){
 	if(serial_uart == NULL) return HAL_ERROR;
-	return HAL_UART_Receive(serial_uart, byte, 1, 0);
+	return HAL_UART_Receive(serial_uart, byte, 1, SERIAL_TIMEOUT_MS);
 }
 
 
 // returns HAL_OK if the bytes were read
 HAL_StatusTypeDef Serial_ReadBytes(uint8_t* bytes, uint16_t length){
 	if(serial_uart == NULL) return HAL_ERROR;
-	return HAL_UART_Receive(serial_uart, bytes, length, 0);
+	return HAL_UART_Receive(serial_uart, bytes, length, SERIAL_TIMEOUT_MS);
 }
 
 
